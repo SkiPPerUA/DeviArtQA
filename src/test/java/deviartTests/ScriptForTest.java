3c83390765e01deb18e531b514993e5ca345b653
Vlad <savchukvi12@gmail.com>
2025-06-04 10:25:07 +0300

    test

---
+++ src/test/java/deviartTests/ScriptForTest.java
@@ -0,0 +1,12 @@
+package deviartTests;
+
+import org.testng.annotations.Test;
+
+@Test
+public class ScriptForTest extends BaseTest{
+
+    public void makeOrderForGetInvoiceOrProforma(){
+        getDB().update("update terraleads_shipping.`order` set status=3, tracker='SWS46900739', tracker_date='2025-06-02 15:22:50.000' where " +
+                "id > 14965796");
+    }
+}
